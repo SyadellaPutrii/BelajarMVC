245eeb94fbdbec591651b06639599957ea4cd084
Syadella Putri <syadellap82@gmail.com>
2024-03-20 09:45:19 +0700

    Pertemuan 2

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,23 @@ class App
 {
     public function __construct()
     {
-        echo 'Berhasil Masuk Ke Class Utama App ! ';
+       $url = $this->parseURL();
+       var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            // -> Ambil value urlnya.
+            $url = $_GET['url'];
+            // -> Buang tanda '/' diakhir url.
+            $url = rtrim($url, '/');
+            //-> Filter url dari karakter2 aneh.
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // -> Pecah url berdasarkan tanda '/' sebagai pemisah
+            $url = explode('/', $url);
+
+            return $url;
+        }
     }
 }
